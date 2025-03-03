9b877c3e80bdc41bd1e1fe8eda2f2774a73390e9
Avanish Singh <107432071+avanishsinghav@users.noreply.github.com>
2025-03-03 19:35:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //your JS code here. If required.
-const output = document.getElementById("output");
+const loadingDiv = document.getElementById("loading");
+const outputDiv = document.getElementById("output");
+const errorDiv = document.getElementById("error");
 const btn = document.getElementById("download-images-button");
 
 const images = [
@@ -7,3 +9,27 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function downloadImage(url){
+	return new Promise((resolve,reject)=>{
+		const img = new Image();
+		img.src = url;
+		img.onload = ()=>resolve(img);
+		img.onerror = ()=>reject(`failed to load image: ${url}`);
+	})
+}
+async function dowmloadImages(){
+	loadingDiv.style.display = "block";
+	output.innerHTML = "";
+	errorDiv.innerHTML = "";
+	try{
+		const images = await Promise.all(images.map(downloadImage));
+		loadingDiv.style.display = "none";
+		images.forEach(img => outputDiv.appendChild(img));
+	}
+	catch(error){
+		loadingDiv.style.display = "none";
+        errorDiv.innerText = error;
+	}
+	downloadImages();
+}
